92d831b3f48e01e7f7de18584106f404a431dd46
Xavier Solano <xaviersolano91@gmail.com>
2023-12-14 16:42:22 -0600

    modified

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function App() {
+function App() {}
     window.onload = function(event) {
         var app = new App();
         window.app = app;
-
     }
 
-    App.prototype.processingButton = function (event ) {
+    App.prototype.processingButton = function (event) {
 
         const btn = event.currentTarget;
-        const carruselList = event.currenTarget.parentNode;
+        const carruselList = event.currentTarget.parentNode;
         const track = event.currentTarget.parentNode.querySelector('#track');
         const carrusel = track.querySelectorAll('.carrusel');
 
@@ -19,23 +18,23 @@ function App() {
         const listWidth = carruselList.offsetWidth;
 
 
-        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) *-1);
+        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) * -1);
         btn.dataset.button == "button-prev" ? prevAction(leftPosition,carruselWidth, track) : nextAction(leftPosition,trackWidth, listWidth, carruselWidth, track);
 
     }
     
         let prevAction = (leftPosition, carruselWidth, track) => {
             if (leftPosition > 0 ) {
-                track.style.left = '${-1 * (leftPosition - carruselWidth)}px';
+                track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
             }
         }
 
         let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
 
             if (leftPosition < (trackWidth - listWidth)) {
-                track.style.left = '${-1 * (leftPosition + carruselWidth)}px';
+                track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
             }
 
 
         }
-} 
+ 
